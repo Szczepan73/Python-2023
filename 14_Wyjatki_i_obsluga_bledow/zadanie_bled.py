a4c352f2f98b433f05af84b138cd8f96bc55c8ce
localadmin <marcin73szczepanski@gmail.com>
2023-12-13 14:52:18 +0100

    update

---
+++ 14_Wyjatki_i_obsluga_bledow/zadanie_bled.py
@@ -0,0 +1,21 @@
+suma = 0
+while True:
+    try:
+        inp = str(input("Podaj liczbe"))
+        for c in inp:
+            suma += int(c)
+        print(suma)
+        if inp == "":
+            break
+    except IndexError as e:
+        print(e)
+    except ValueError as e:
+        print("In Value Error")
+        print(e)
+    except Exception as e:
+        print("In Exception")
+        print(e)
+    else:
+        break
+    finally:
+        print("Finally")
