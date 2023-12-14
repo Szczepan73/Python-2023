2174d4e3e18e4c5630576a04429cc32acdaf1f0f
Marcin <marcin73szczepanski@gmail.com>
2023-12-14 22:13:41 +0100

    nowy

---
+++ 01_Srodowisko_pracy/moje zadania.py
@@ -0,0 +1,16 @@
+# def disemvowel(string_):
+
+
+
+
+        inp = str("AAALLLuuiizztrtYYY")
+
+        for n in 'aeiouAEIOU':
+
+            inp = inp.replace(n, '')
+            #print(inp)
+            #print(nowy)
+        print(inp)
+#    return string_
+
+
