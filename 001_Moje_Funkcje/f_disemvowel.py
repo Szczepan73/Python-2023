efdf1f9721ad4d485f8ee3bcccf0b890160f0a92
Marcin <marcin73szczepanski@gmail.com>
2023-12-18 16:36:30 +0100

    nowy

---
+++ 001_Moje_Funkcje/f_disemvowel.py
@@ -0,0 +1,5 @@
+def disemvowel(s):
+
+    for n in 'aeiouAEIOU':
+        s = s.replace(n, '')
+    return s
---
+++ 01_Srodowisko_pracy/Zadanie_2.py
@@ -0,0 +1,8 @@
+def disemvowel(s):
+
+    for n in 'aeiouAEIOU':
+        s = s.replace(n, '')
+    return s
+
+
+
